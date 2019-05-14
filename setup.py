b3e60840a4807442f95f36db5bc5819f76678808
Sam Garrett <samgarrett@iheartmedia.com>
2019-05-14 13:31:34 -0400

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ tools = [
 
 setup(
     name='ihrpi',
-    version='1.0.3',
+    version='1.0.4',
     author='Sam Garrett',
     author_email='samgarrett@iheartmedia.com',
     description='iHeart private packaging tools & index.',
